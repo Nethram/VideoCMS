2c8f1c82216bb29a3fedbea95e5c76b120d98847
Nethram <nethramllc@gmail.com>
2016-01-05 15:31:40 +0530

    link to index.php fixed

--- playlist.php
+++ playlist.php
@@ -71,7 +71,7 @@
 
       <header class="main-header">
         <!-- Logo -->
-        <a href="index2.html" class="logo">
+        <a href="index.php" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b>V</b>CMS</span>
           <!-- logo for regular state and mobile devices -->
